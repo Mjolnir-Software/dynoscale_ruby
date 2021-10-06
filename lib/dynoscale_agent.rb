a4287802f379980ef8cb0be1511f5e63fbf018da
Eric Abrahamsn <ericthredsbeard@gmail.com>
2021-10-06 14:13:43 -0400

    Gem initial commit

---
+++ lib/dynoscale_agent.rb
@@ -0,0 +1,6 @@
+require "dynoscale_agent/version"
+
+module DynoscaleAgent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dynoscale_agent/version.rb
@@ -0,0 +1,3 @@
+module DynoscaleAgent
+  VERSION = "0.1.0"
+end
