cb8ca192b93d1dcc6099f48a799502a287b070d9
Eric Abrahamsn <ericthredsbeard@gmail.com>
2021-10-06 14:14:47 -0400

    Load required files

--- lib/dynoscale_agent.rb
+++ lib/dynoscale_agent.rb
@@ -1,6 +1,8 @@
-require "dynoscale_agent/version"
+# frozen_string_literal: true
+
+require 'dynoscale_agent/version'
+require 'dynoscale_agent/railtie' if defined?(Rails::Railtie) && Rails::Railtie.respond_to?(:initializer)
 
 module DynoscaleAgent
   class Error < StandardError; end
-  # Your code goes here...
 end
