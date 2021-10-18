5beed1a9708aa44650bc1b8624c54d849b002b80
Eric Abrahamsn <ericthredsbeard@gmail.com>
2021-10-18 14:54:53 -0400

    Fix request body calc

--- lib/dynoscale_agent/request_calculator.rb
+++ lib/dynoscale_agent/request_calculator.rb
@@ -12,7 +12,7 @@ module DynoscaleAgent
       request_start_string = request_start.match(/([0-9])+/).try(:[], 0)
       start_at = Time.at(request_start_string.to_i / 1000)
       puts "HTTP_X_REQUEST_START: #{@env['HTTP_X_REQUEST_START']} r@env['puma.request_body_wait']: #{@env['puma.request_body_wait']} request_start_string: #{request_start_string}"
-      (Time.now - start_at).to_i - request_body_wait.to_i
+      (Time.now - start_at).to_i + request_body_wait.to_i
     end
   end
   class MissingRequestStartError < StandardError
